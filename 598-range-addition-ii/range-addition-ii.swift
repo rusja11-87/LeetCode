24f70a1da88cd117e2d1521df48b9f3d5411693f
rusja11-87 <143793408+rusja11-87@users.noreply.github.com>
2025-05-22 14:48:53 +0300

    Time: 0 ms (100.00%) | Memory: 19.7 MB (50.00%) - LeetSync

---
+++ 598-range-addition-ii/range-addition-ii.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func maxCount(_ m: Int, _ n: Int, _ ops: [[Int]]) -> Int {
+        var minAi = m
+        var minBi = n
+        
+        for op in ops {
+            minAi = min(minAi, op[0])
+            minBi = min(minBi, op[1])
+        }
+        return minAi * minBi
+    }
+}
