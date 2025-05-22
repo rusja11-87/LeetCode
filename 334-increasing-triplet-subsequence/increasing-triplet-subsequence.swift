033a9d3c4f41a32eb657f77be3f7fc1c177c7d9d
rusja11-87 <143793408+rusja11-87@users.noreply.github.com>
2025-05-22 15:37:35 +0300

    Time: 0 ms (100.00%) | Memory: 33.8 MB (41.36%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func increasingTriplet(_ nums: [Int]) -> Bool {
+        var first = Int.max
+        var second = Int.max
+        for i in nums {
+            if i <= first {
+                first = i
+            } else if i <= second {
+                second = i
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+}
