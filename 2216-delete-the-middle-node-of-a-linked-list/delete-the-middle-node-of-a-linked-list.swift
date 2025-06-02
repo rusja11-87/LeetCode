bf21e09c36ab79edcced33d2cd10c70496fbd9e5
rusja11-87 <143793408+rusja11-87@users.noreply.github.com>
2025-06-02 11:10:36 +0300

    Time: 57 ms (26.67%) | Memory: 31.6 MB (7.50%) - LeetSync

---
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.swift
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func deleteMiddle(_ head: ListNode?) -> ListNode? {
+        if head?.next == nil {
+            return nil
+        }
+        var slow = head
+        var fast = head
+        var prev: ListNode?
+        while fast?.next != nil {
+            prev = slow
+            slow = slow?.next!
+            fast = fast?.next?.next
+        }
+        prev?.next = slow?.next
+        return head
+    }
+}
