0e6474ec45577e22e084c0a0a7a9fc75d9a72416
rusja11-87 <143793408+rusja11-87@users.noreply.github.com>
2025-06-03 12:47:46 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (5.34%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+       guard let root = root else {
+            return 0
+        }
+        let leftDepth = maxDepth(root.left)
+        let rightDepth = maxDepth(root.right)
+        
+        return max(leftDepth, rightDepth) + 1
+        
+        return 0
+    }
+}
